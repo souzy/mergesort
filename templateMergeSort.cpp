c868330d715269a081544c196a517470e0bf883c
souzy <32474135+souzy@users.noreply.github.com>
2017-10-16 20:33:34 -0700

    Update templateMergeSort.cpp

--- templateMergeSort.cpp
+++ templateMergeSort.cpp
@@ -2,7 +2,6 @@
 #include <cassert>
 
 using namespace std;
-
        // s = start of range to sort
        // e = end of range to sort
 template <class T>
@@ -11,16 +10,12 @@ void msort(T a[], int x[], int s, int e) {
         int mid;
 	if (s >= e)
 	{
-	return;
-	    
+		return;  
 	}
-
 	 mid=(s+e)/2;
 	 msort(a, x, s, mid);
 	 msort(a, x, mid+1, e);
-	 
 	// We put mid+1 because we want to get to 4 and to end in order to sort.
-
 	int i;
 	int j;
 	int k;
@@ -29,21 +24,20 @@ void msort(T a[], int x[], int s, int e) {
 	j = mid + 1;
 	// Merge the two parts into x[].
 	while (i <= mid && j <= e)
-	  {
-	 if (a[i] < a[j])
+	{
+	 	if (a[i] < a[j])
 		{
 			x[k] = a[i];
 			k++;
 			i++;
 		}
-	else
+		else
 		{
 			x[k] = a[j];
 			k++;
 			j++;
 		}
 	}
- 
 	// We have put all remaining values from i to mid into x[].
 	while (i <= mid)
 	{
@@ -51,7 +45,6 @@ void msort(T a[], int x[], int s, int e) {
 		k++;
 		i++;
 	}
- 
 	// We have put all remaining values from j to high into x[].
 	while (j <= e)
 	{
@@ -60,14 +53,11 @@ void msort(T a[], int x[], int s, int e) {
 		j++;
        
 	}
-	
 	for (int i = s; i <= e; i++)
 	{
 		a[i] = x[i-s];
 	}
-     
 }
- 
 template <class T>
 void mergesort(T a[], int n) {
         int * x = new int[n];
@@ -79,11 +69,10 @@ bool sorted(T a[], int n) {
         for (int i = 0; i < n - 1; ++i)
     {
         if (a[i] > a[i + 1])
-	return false;
+		return false;
     }
         return true;
 }
-
 int main(int argc, char * args[]) {
 	int a[1000];
 	for (int i = 0; i < 1000; ++i) a[i] = -50 + rand() % 100;
@@ -102,6 +91,5 @@ int main(int argc, char * args[]) {
 	int d[] = { 1, 2, 3, 4, 5 };
 	mergesort(d, 5);
 	assert(sorted(d, 5));
-	
 	cout << "All tests passed." << endl;	
 }
